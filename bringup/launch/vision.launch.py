8e83a560c11ae86fe81b3d610972b77732b04928
Astro-Coco <colinc131@gmail.com>
2026-05-16 18:55:25 -0400

    vision launch files

--- bringup/launch/vision.launch.py
+++ bringup/launch/vision.launch.py
@@ -5,15 +5,23 @@ from launch.actions import IncludeLaunchDescription
 def generate_launch_description():
     ld = LaunchDescription()
 
-    rgb_yolo = Node(
+    tensor_yolo = Node(
         package="vision",
-        executable="rgb_yolo",
-        name="rgb_yolo",
+        executable="tensorRt_yolo",
+        name="tensor_yolo",
         parameters=[{
             'topic_name': "/zed/zed_node/rgb/color/rect/image",
         }],
     ) 
 
-    ld.add_action(rgb_yolo)
+    fps_test = Node(
+        package="vision",
+        executable="fps_counter",
+        name="fps_counter"
+    ) 
+
+
+    ld.add_action(tensor_yolo)
+    ld.add_action(fps_test)
 
     return ld
