3f6484880d5f935ee2991db8d8e11c3f7e741b34
Astro-Coco <colinc131@gmail.com>
2026-05-19 22:50:31 -0400

    change the model for phase 2

--- bringup/launch/vision.launch.py
+++ bringup/launch/vision.launch.py
@@ -5,23 +5,29 @@ from launch.actions import IncludeLaunchDescription
 def generate_launch_description():
     ld = LaunchDescription()
 
-    tensor_yolo = Node(
+    # tensor_yolo = Node(
+    #     package="vision",
+    #     executable="tensorRt_yolo",
+    #     name="tensor_yolo",
+    #     parameters=[{
+    #         'topic_name': "/zed/zed_node/rgb/color/rect/image",
+    #     }],
+    # ) 
+
+    rgb_yolo = Node(
         package="vision",
         executable="tensorRt_yolo",
-        name="tensor_yolo",
-        parameters=[{
-            'topic_name': "/zed/zed_node/rgb/color/rect/image",
-        }],
-    ) 
-
+        name="rgb_yolo"
+    )
+    
     fps_test = Node(
         package="vision",
         executable="fps_counter",
         name="fps_counter"
     ) 
 
-
-    ld.add_action(tensor_yolo)
+    # ld.add_action(tensor_yolo)
+    ld.add_action(rgb_yolo)
     ld.add_action(fps_test)
 
     return ld
